46dc7a5abc11a3b104b98327bf9ec4411cf8842b
Aleki <karuri1.k1@gmail.com>
2023-04-06 13:25:47 -0700

    changes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,8 +11,8 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
-		return (1);
-		return (check_pal(s, 0, _strlen_recursion(s)));
+	return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - returns the length of a string
@@ -23,8 +23,8 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
-		return (1 + _strlen_recursion(s + 1));
+	return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
@@ -37,8 +37,8 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
-		return (0);
+	return (0);
 		if (i >= len)
-			return (1);
-			return (check_pal(s, i + 1, len - 1));
+		return (1);
+		return (check_pal(s, i + 1, len - 1));
 }
